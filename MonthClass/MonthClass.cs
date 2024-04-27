802c2f48a8f798405a79143a33f850645cf2d97c
Nathaniel Rivera <52226518+DeveloperSpoot@users.noreply.github.com>
2024-04-27 09:42:07 +0000

    Month stuff.

--- MonthClass/MonthClass.cs
+++ MonthClass/MonthClass.cs
@@ -2,5 +2,25 @@
 {
     internal class MonthClass
     {
+        private Dictionary<int, string> month = new Dictionary<int, string>(){
+            {1, "Janaury"}
+        };
+        public int MonthNumber {get; init;}
+        public string MonthName {get; init;}
+
+        public MonthClass(){
+            MonthNumber = 1;
+        }
+
+        public MonthClass(int monthNumber){
+            if(monthNumber < 1 || monthNumber > 12){
+                MonthNumber = 1;
+            }else{
+                MonthNumber = monthNumber;
+            }
+
+            DateTime newDT = new DateTime(2024, 1, 1);
+            MonthName = newDT.ToString("m");
+        }
     }
 }
